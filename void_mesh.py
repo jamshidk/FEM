86d93ba26d38bd397f5536d472178b9e5c7268f1
Jamshid Kavosi <45982012+jamshidk@users.noreply.github.com>
2024-11-07 20:09:55 -0800

    Circular_Void_mesh.py

--- void_mesh.py
+++ void_mesh.py
@@ -67,8 +67,8 @@ def void_mesh(d1, d2, p, m, R, element_type):
             dx=(coor22[m*(p+1)+j-1,0]-coor22[j-1,0])/m
             dy=(coor22[m*(p+1)+j-1,1]-coor22[j-1,1])/m
             
-            coor11[i*(p+1)+j-1,0]=coor22[(i-1)*(p+1)+j-1,0]+dx
-            coor11[i*(p+1)+j-1,1]=coor22[(i-1)*(p+1)+j-1,1]+dy
+            coor22[i*(p+1)+j-1,0]=coor22[(i-1)*(p+1)+j-1,0]+dx
+            coor22[i*(p+1)+j-1,1]=coor22[(i-1)*(p+1)+j-1,1]+dy
             
     ### region 3 ###
     coor33=np.zeros([(p-1)*(m+1),PD])
@@ -116,5 +116,51 @@ def void_mesh(d1, d2, p, m, R, element_type):
             
     #### RE-ORDERING THE NODES ####
     
+    for i in range(1, m+2):
+        NL[(i-1)*4*p:i*4*p,:]=np.vstack([coor11[(i-1)*(p+1):i*(p+1),:],
+                                         coor44[(i-1)*(p-1):i*(p-1),:],
+                                         np.flipud(coor22[(i-1)*(p+1):i*(p+1),:]),
+                                         np.flipud(coor33[(i-1)*(p-1):i*(p-1),:])])
+
     ### ELEMENT ####
     
+    EL=np.zeros([NoE, NPE])
+    
+    for i in range(1, m+1):
+        for j in range(1,4*p+1):
+            if j==1:
+                EL[(i-1)*(4*p)+j-1,0]=(i-1)*(4*p)+1
+                EL[(i-1)*(4*p)+j-1,1]=EL[(i-1)*(4*p)+j-1,0]+1
+                EL[(i-1)*(4*p)+j-1,3]=EL[(i-1)*(4*p)+j-1,0]+4*p
+                EL[(i-1)*(4*p)+j-1,2]=EL[(i-1)*(4*p)+j-1,3]+1
+                
+            elif j==4*p:
+                EL[(i-1)*(4*p)+j-1,0]=i*(4*p)
+                EL[(i-1)*(4*p)+j-1,1]=(i-1)*(4*p)+1
+                EL[(i-1)*(4*p)+j-1,2]= EL[(i-1)*(4*p)+j-1,0] +1
+                EL[(i-1)*(4*p)+j-1,3]= EL[(i-1)*(4*p)+j-1,0] +4*p
+                
+            else:
+                EL[(i-1)*(4*p)+j-1,0]=EL[(i-1)*(4*p)+j-2,1]
+                EL[(i-1)*(4*p)+j-1,3]=EL[(i-1)*(4*p)+j-2,2]
+                EL[(i-1)*(4*p)+j-1,2]=EL[(i-1)*(4*p)+j-1,3]+1
+                EL[(i-1)*(4*p)+j-1,1]=EL[(i-1)*(4*p)+j-1,0]+1
+                
+        if element_type == 'D2TR3N':
+            NPE_new=3
+            NoE_new=2*NoE
+            EL_new=np.zeros([NoE_new,NPE_new])
+            
+            for i in range(1, NoE+1):
+                EL_new[2*(i-1),0]=EL[i-1,0]
+                EL_new[2*(i-1),1]=EL[i-1,1]
+                EL_new[2*(i-1),2]=EL[i-1,2]
+                #for the second trianle element
+                EL_new[2*(i-1)+1,0]=EL[i-1,0]
+                EL_new[2*(i-1)+1,1]=EL[i-1,2]
+                EL_new[2*(i-1)+1,2]=EL[i-1,3]
+            EL=EL_new
+            
+    EL=EL.astype(int)  
+             
+    return NL, EL
